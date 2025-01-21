8b12d6fd516d0873caa107cf9c8ab65a7479eef8
baiching <bachmarma@gmail.com>
2025-01-21 01:40:19 +0100

    Added blank Frontend side

---
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/FplEssentialsApplication.java
@@ -0,0 +1,13 @@
+package com.baiching.fpl_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplEssentialsApplication.class, args);
+	}
+
+}
