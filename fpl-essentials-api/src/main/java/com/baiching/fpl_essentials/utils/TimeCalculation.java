6e7bedfbb058a2981c85a9a26ce8269754b4dc98
baiching <bachmarma@gmail.com>
2025-02-03 12:18:14 +0100

    returns Duration object where it stores how many days remains to current date until input date

--- fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/utils/TimeCalculation.java
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/utils/TimeCalculation.java
@@ -1,5 +1,6 @@
 package com.baiching.fpl_essentials.utils;
 
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -9,5 +10,16 @@ public class TimeCalculation {
         OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString);
 
         return offsetDateTime.atZoneSimilarLocal(ZoneId.systemDefault());
+    }
+
+    public Duration getTimeLeft(ZonedDateTime futureDateTime){
+        /*
+        To access values correctly:
+        *duration.toDays();
+        *duration.toHours() % 24;
+        *duration.toMinutes() % 60;
+        *duration.getSeconds() % 60;
+         */
+        return Duration.between(ZonedDateTime.now(), futureDateTime);
     }
 }
