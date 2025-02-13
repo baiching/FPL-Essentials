0964a1be000a4abd7def807f7fb5b7bba2dc1712
baiching <bachmarma@gmail.com>
2025-02-13 18:58:59 +0100

    Added finadAll method in repository which will return A list

--- fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/repository/GameWeekDeadlineRepository.java
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/repository/GameWeekDeadlineRepository.java
@@ -3,7 +3,10 @@ package com.baiching.fpl_essentials.repository;
 import com.baiching.fpl_essentials.model.Gameweek;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GameWeekDeadlineRepository extends JpaRepository<Gameweek, Long> {
     Gameweek findByName(String name);
     Gameweek findById(int id);
+    List<Gameweek> findAll();
 }
