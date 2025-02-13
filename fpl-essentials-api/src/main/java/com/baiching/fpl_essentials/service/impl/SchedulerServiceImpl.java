472db6fd5cd950a6e3c8ebab315568fc85b96031
baiching <bachmarma@gmail.com>
2025-02-13 22:22:15 +0100

    made adjustments to for scheduling the tasks

--- fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/impl/SchedulerServiceImpl.java
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/impl/SchedulerServiceImpl.java
@@ -1,31 +1,33 @@
 package com.baiching.fpl_essentials.service.impl;
 
-import com.baiching.fpl_essentials.scheduledJobs.ScheduledJob;
-import jakarta.annotation.PostConstruct;
+import com.baiching.fpl_essentials.scheduledJobs.EmailJob;
+import com.baiching.fpl_essentials.service.SchedulerService;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Service
-public class SchedulerServiceImpl {
+public class SchedulerServiceImpl implements SchedulerService {
     @Autowired
     private SchedulerFactoryBean schedulerFactoryBean;
 
-    public void scheduleJob(String jobName, ZonedDateTime time) throws SchedulerException {
+    @Override
+    public void scheduleJob(String taskName, String email, ZonedDateTime emailTime) throws SchedulerException {
         Scheduler scheduler = schedulerFactoryBean.getScheduler();
 
-        JobDetail job = JobBuilder.newJob(ScheduledJob.class)
-                .withIdentity("simpleTrigger", "group1")
+        JobDetail job = JobBuilder.newJob(EmailJob.class)
+                .withIdentity(taskName + "EmailJob", "emailGroup")
+                .usingJobData("taskName", taskName)
+                .usingJobData("email", email)
                 .build();
 
         Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("simpleTrigger", "group1")
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInSeconds(10)
-                        .repeatForever())
+                .withIdentity(taskName + "EmailTrigger", "emailGroup")
+                .startAt(Date.from(emailTime.toInstant()))
                 .build();
 
         scheduler.scheduleJob(job, trigger);
