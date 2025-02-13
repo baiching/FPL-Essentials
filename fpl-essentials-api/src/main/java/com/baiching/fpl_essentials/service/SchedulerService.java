732726f06618a344e4b6372f6d31b5809956e3d9
baiching <bachmarma@gmail.com>
2025-02-13 22:21:17 +0100

    Added parameters in scheduleJob method

--- fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/SchedulerService.java
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/SchedulerService.java
@@ -1,5 +1,9 @@
 package com.baiching.fpl_essentials.service;
 
+import org.quartz.SchedulerException;
+
+import java.time.ZonedDateTime;
+
 public interface SchedulerService {
-    void scheduleJob();
+    void scheduleJob(String taskName, String email, ZonedDateTime emailTime) throws SchedulerException;
 }
