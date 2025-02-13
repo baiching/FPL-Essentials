2523a69d90db5584bc0df2b23b85b52fc7236735
baiching <bachmarma@gmail.com>
2025-02-13 21:24:08 +0100

    added a service to send email 30 minutes before deadline

---
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/impl/FplDeadlineServiceImpl.java
@@ -0,0 +1,38 @@
+package com.baiching.fpl_essentials.service.impl;
+
+import com.baiching.fpl_essentials.model.Gameweek;
+import com.baiching.fpl_essentials.repository.GameWeekDeadlineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Service
+public class FplDeadlineServiceImpl implements com.baiching.fpl_essentials.service.FplDeadlineService {
+    @Autowired
+    GameWeekDeadlineRepository repository;
+
+    @Override
+    public void deadlineReminder() {
+        List<Gameweek> overallData = repository.findAll();
+
+        ZonedDateTime currentTime = ZonedDateTime.now();
+
+        for (Gameweek gameweek : overallData) {
+            ZonedDateTime deadlineDate = ZonedDateTime.parse(gameweek.getDeadline_time());
+
+            if (deadlineDate.isAfter(currentTime)) {
+                ZonedDateTime emailTime = deadlineDate.minusMinutes(30);
+
+                scheduleEmail(gameweek.getName(), "user@example.com", emailTime);
+            } else {
+                System.out.println("Task '" + gameweek.getName() + "' has already passed its deadline.");
+            }
+
+        }
+    }
+
+    private void scheduleEmail(String name, String mail, ZonedDateTime emailTime) {
+    }
+}
