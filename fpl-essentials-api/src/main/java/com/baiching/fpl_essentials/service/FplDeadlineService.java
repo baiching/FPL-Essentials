30b0296c88532f98725ecb5a0282e922dea6555e
baiching <bachmarma@gmail.com>
2025-02-13 22:18:28 +0100

    changed the paramenters in the interface

--- fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/FplDeadlineService.java
+++ fpl-essentials-api/src/main/java/com/baiching/fpl_essentials/service/FplDeadlineService.java
@@ -1,5 +1,7 @@
 package com.baiching.fpl_essentials.service;
 
+import org.quartz.SchedulerException;
+
 public interface FplDeadlineService {
-    void deadlineReminder();
+    void deadlineReminder() throws SchedulerException;
 }
